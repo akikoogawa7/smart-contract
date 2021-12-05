76bc046eca79bef2301afef33e81e21c9a908992
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-12-05 19:25:48 +0000

    Modified migrations

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,10 +3,10 @@ pragma solidity >=0.4.11;
 contract Election {
     // Store candidate
     // Read candidate
-    string public candidate;
-    
+    string public candidate = 'Candidate 1';
+
     // Constructor 
-    function Election () public {
-        candidate = 'Candidate 1';
+    constructor() public {
+        candidate = candidate;
     }
 }
